1169d3849412f67a9238bf6f396dbf072fe212bd
Erica Perkins <38138378+madebyknight@users.noreply.github.com>
2018-04-06 13:06:20 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>ND Kutz Barbershop</title>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Barrio|Calligraffitti|Great+Vibes|Neucha|Roboto|Philosopher|Sansita|Anton|Kumar+One|Poiret+One|Paprika|Abril+Fatface|Shrikhand');
+        </style>
+        <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALBN3r94mRU7Pjw_xwEgk_LDEaqsHk6ks&callback=initMap">
+    </script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="home.js" type="text/javascript"></script>
+   </head>
+ </html>
